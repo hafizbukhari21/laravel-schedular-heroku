91f24cd51c1b5ddd946de1b1e5a587f34b2f03ef
M. Hafiz Bukhari <53202815+hafizbukhari21@users.noreply.github.com>
2023-07-28 20:25:41 +0700

    maeiler

--- app/Http/Controllers/mailercon.php
+++ app/Http/Controllers/mailercon.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Mail\mailer;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
@@ -14,9 +13,6 @@ class mailercon extends Controller
     public function send(){
         {
             Mail::to("hafiz.bukhari@hotmail.com")->send(new mailer());
-            // $user = new User();
-            // $user->name = Str::uuid();
-            // $user->save();
         }
     }
 }
