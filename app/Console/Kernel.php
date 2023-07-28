348cada332a9e7ee73abae337380fd04ed4340a2
M. Hafiz Bukhari <53202815+hafizbukhari21@users.noreply.github.com>
2023-07-28 19:43:21 +0700

    init

---
+++ app/Console/Kernel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console;
+
+use App\Mail\mailer;
+use App\Models\User;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        // $schedule->command('inspire')->hourly();
+        $schedule->job(function(){
+            Mail::to("hafiz.bukhari@hotmail.com")->send(new mailer());
+       
+        })->everyMinute();
+        //php artisan schedule:work 
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
